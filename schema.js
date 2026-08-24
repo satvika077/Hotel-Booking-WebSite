d7f7d5bd1bd2b3eda2688eacea0538bcf85f7bdb
N.Sai Satvika <nsaisatvika@gmail.com>
2026-08-24 22:26:58 +0530

    Add latitude and longitude validation to schema

--- schema.js
+++ schema.js
@@ -6,8 +6,9 @@ module.exports.listingSchema=Joi.object({
         location: Joi.string().required(),
         country:Joi.string().required(),
         price:Joi.number().required().min(0),
-        image:Joi.string().allow("",null)
-
+        image:Joi.string().allow("",null),
+        latitude:Joi.number().required().min(-90).max(90),
+        longitude:Joi.number().required().min(-180).max(180)
     }).required()
 });
 module.exports.reviewSchema= Joi.object({
